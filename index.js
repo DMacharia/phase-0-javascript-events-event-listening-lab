778915f17dbdb0d67a14ab8f10239684a3f2f852
Daniel Macharia <danielhuios@gmail.com>
2022-08-24 17:53:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+////Approach 1
+// const input = document
+//   .getElementById("input")
+//   .addEventListener("click", addingEventListener);
+
+// function addingEventListener() {
+//   alert("I have been clicked");
+// }
+
+////Approach 2
+// function addingEventListener() {
+//   const input = document.getElementById("input");
+//   alert("I was clicked!");
+// }
+// input.addEventListener("click", addingEventListener);
+
+//Approach 3
 function addingEventListener() {
+  const input = document.getElementById("input");
+  input.addEventListener("click", addingEventListener);
+  alert("I was clicked!");
 }
+addingEventListener();
